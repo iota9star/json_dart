5b38a3b78ee3f64d17c8508b6b355019f89d96fe
iota9star <iota9star@foxmail.com>
2023-05-26 18:53:00 +0800

    🔥 Reduce assets size

--- scripts/flutter.sh
+++ scripts/flutter.sh
@@ -3,5 +3,5 @@
 git clone --depth 1 -b stable https://github.com/flutter/flutter.git flutter-sdk
 export PATH="$PATH:`pwd`/flutter-sdk/bin"
 cd apps/jsond
-flutter build web --release --web-renderer canvaskit
+flutter build web --release --web-renderer canvaskit --tree-shake-icons
 dart scripts/web.dart
